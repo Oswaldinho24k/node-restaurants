0ac6b5756f5d41d672d65a8e7933b2ebc9a76fb2
diurivj <diurivj@icloud.com>
2018-09-18 09:48:22 -0500

    putita

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+const auth  = require('./routes/auth');
 app.use('/', index);
+app.use('/', auth);
+
 
 
 module.exports = app;
